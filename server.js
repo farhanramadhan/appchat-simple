e0109be04c8252caf9b24d67cd25ece10eecff46
Farhan Khair <farhanramadhansk@gmail.com>
2024-01-26 13:40:52 +0700

    feat: init commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origins: ["http://localhost:8080"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hey Socket.io</h1>");
+});
+
+io.on("connection", (socket) => {
+  let token = socket.handshake.auth.token;
+
+  socket.join("myRandomChatRoomId");
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("my message", (msg) => {
+    io.emit("my broadcast", `server: ${(msg, token)}`);
+  });
+
+  socket.on("join", (roomName) => {
+    socket.join(roomName);
+  });
+
+  socket.on("message", ({ message, roomName }, callback) => {
+    const outgoingMessage = {
+      name: token,
+      id: token,
+      message,
+    };
+
+    socket.to(roomName).emit("message", outgoingMessage);
+    callback({
+      status: "ok",
+    });
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
